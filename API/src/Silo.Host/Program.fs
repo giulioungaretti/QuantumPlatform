4143c4b6ef6ddcb1f92f678de110de08fc5af3cf
Giulio Ungaretti <giulioungaretti@me.com>
2019-04-03 21:54:21 +0200

    fun: try out generic host from net core

--- API/src/Silo.Host/Program.fs
+++ API/src/Silo.Host/Program.fs
@@ -1,54 +1,58 @@
-﻿open Orleans
-open Orleans.Hosting
-open System
-open FSharp.Control.Tasks
-open System.Threading;
-
+﻿open System
+open System.Net
+open System.Threading.Tasks
 open Grains
 open Interfaces
-open System.Runtime.Loader
 open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Configuration
+open Orleans
 open Orleans.Configuration
+open Orleans.Hosting
 
 
-let buildSiloHost () =
-      let builder = SiloHostBuilder()
-      builder
-        .Configure<ClusterOptions>(fun (options:ClusterOptions) -> 
-              options.ClusterId <- "orleans-dev-docker"; 
-              options.ServiceId <- "FsharpOrleansDev")
-        .ConfigureEndpoints(siloPort=11111, gatewayPort=30000)
-        .UseAzureStorageClustering(fun (options:AzureStorageClusteringOptions)-> 
-               options.ConnectionString <- connectionString )
-        .ConfigureApplicationParts(fun parts ->
-            parts.AddApplicationPart(typeof<HelloGrain>.Assembly)
-                  .AddApplicationPart(typeof<IHello>.Assembly)
-                  .WithCodeGeneration() |> ignore)
-        .ConfigureLogging(fun logging -> logging.AddConsole() |> ignore)
-        .Build()
 
+[<CLIMutable>]
+type Config =
+    {
+        OrleansTableURL : string
+    }
+
+let buildSiloHost (context: Microsoft.Extensions.Hosting.HostBuilderContext) (siloBuilder:ISiloBuilder) =
+       let conf  = context.Configuration
+       let url = conf.GetSection("OrleansTableUrl").Get<Config>().OrleansTableURL
+       siloBuilder
+         .Configure<ClusterOptions>(fun (options:ClusterOptions) -> 
+               options.ClusterId <- "orleans-dev-docker"; 
+               options.ServiceId <- "FsharpOrleansDev")
+         .ConfigureEndpoints(siloPort=11111, gatewayPort=30000)
+         .UseAzureStorageClustering(fun (options:AzureStorageClusteringOptions)-> 
+                options.ConnectionString <- url )
+         .ConfigureApplicationParts(fun parts ->
+             parts.AddApplicationPart(typeof<HelloGrain>.Assembly)
+                   .AddApplicationPart(typeof<IHello>.Assembly)
+                   .WithCodeGeneration() |> ignore)
+      |> ignore
+
+let configureAppConfiguration (context: Microsoft.Extensions.Hosting.HostBuilderContext) (config: IConfigurationBuilder) =  
+    config
+       .AddJsonFile("appsettings.json",false,true)
+       .AddJsonFile(sprintf "appsettings.%s.json" context.HostingEnvironment.EnvironmentName ,true, true)
+       .AddEnvironmentVariables() |> ignore
+    let env = context.HostingEnvironment
+    (match env.IsDevelopment() with
+    | true -> 
+        config.AddUserSecrets<Config>() |> ignore
+    | false -> 
+        ()
+    )
 
 [<EntryPoint>]
 let main _ =
-    let siloStopped = new ManualResetEvent(false);
-    let host = buildSiloHost ()
-    let start = async {
-      Async.AwaitTask (host.StartAsync ())
-      |> ignore
-      printfn "started silo"
-    }
-
-    let shutdonw = async {
-        Async.AwaitTask ( host.StopAsync () ) |> ignore
-        printfn "stopping silo"
-        siloStopped.Set () |> ignore
-    }
-    System.Runtime.Loader.AssemblyLoadContext.Default.add_Unloading(fun _ -> 
-      printfn "unloading assembly"
-      Async.RunSynchronously shutdonw
-      siloStopped.WaitOne() |> ignore
-    )
-    Async.RunSynchronously start
-    siloStopped.WaitOne() |> ignore
-    printfn "Exiting silo host"
-    0
+      HostBuilder()
+       .UseOrleans(buildSiloHost)
+       .ConfigureAppConfiguration(configureAppConfiguration)
+       .ConfigureLogging(fun logging -> logging.AddConsole() |> ignore)
+       .Build()
+       .RunAsync() |> ignore
+      0
