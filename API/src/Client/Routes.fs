b56d08a3a939505eef6789de0797fa871606870a
Giulio Ungaretti <giulioungaretti@me.com>
2019-04-05 14:02:12 +0200

    feat: more  route types

--- API/src/Client/Routes.fs
+++ API/src/Client/Routes.fs
@@ -1,6 +1,7 @@
 module Routes
 
 open Elmish.Browser.UrlParser
+open Fable.Import.Browser
 
 [<RequireQualifiedAccess>]
 type SampleRoute =
@@ -12,12 +13,26 @@ type Route =
     | Sample of SampleRoute
 
 
+let hashPrefix = sprintf "#%s"
+
+[<Literal>]
+let home = "home"
+
+[<Literal>]
+let sample = "sample"
+
+[<Literal>]
+let newSample = "new"
+
+let combine xs = List.fold (sprintf "%s/%s") "" xs
+
 let toString route =
     match  route with
-    | Route.Home -> "#/home"
+    | Route.Home -> home |> hashPrefix
     | Route.Sample subRoute ->
         match subRoute with 
-            | SampleRoute.New -> "#/sample/new"
+            | SampleRoute.New -> combine [sample
+                                          newSample] |> hashPrefix
 
 let subRoute =
     oneOf [
@@ -31,6 +46,29 @@ let route : Parser<Route->Route,Route> =
         map Route.Sample (s "sample" </> subRoute)
     ]
 
-
 let href =
     toString >> Fable.Helpers.React.Props.Href
+
+
+
+
+// type alias to browsers navigation event 
+[<Literal>]
+let private NavigationEvent = "NavigationEvent"
+
+// go to a an unsafe string url
+let newUrl (newUrl : string) : Elmish.Cmd<_> =
+    [ fun _ -> 
+        history.pushState ((), "", newUrl)
+        let ev = document.createEvent_CustomEvent()
+        ev.initCustomEvent (NavigationEvent, true, true, obj())
+        window.dispatchEvent ev |> ignore ]
+
+// go to safer url
+let navigate xs : Elmish.Cmd<_> =
+    let nextUrl = hashPrefix (combine xs)
+    [ fun _ -> 
+        history.pushState ((), "", nextUrl)
+        let ev = document.createEvent_CustomEvent()
+        ev.initCustomEvent (NavigationEvent, true, true, obj())
+        window.dispatchEvent ev |> ignore ]
