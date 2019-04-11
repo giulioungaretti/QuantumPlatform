9ad25db6174c726fd01d2d60aa49f2db59bf20c3
Giulio Ungaretti <giulioungaretti@me.com>
2019-04-11 18:59:25 +0200

    fix: make urls literals

--- src/Shared/URL.fs
+++ src/Shared/URL.fs
@@ -5,7 +5,9 @@ module URL=
     [<Literal>]
     let api = "/api"
 
+    [<Literal>]
     let sample = "/sample"
+    [<Literal>]
     let samples = "/samples"
 
     let apiURL s:string = sprintf "/api%s" s
