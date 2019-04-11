6c554c8a14c776a3508a0d77986637eab485e2bd
Giulio Ungaretti <giulioungaretti@me.com>
2019-04-11 18:59:39 +0200

    fix: format

--- src/Grains/GrainState.fs
+++ src/Grains/GrainState.fs
@@ -6,18 +6,16 @@ open Interfaces
 
 open Shared
 
-
-
 // satate
 [<Serializable>]
 type SampleHolder(sample : Sample option) =
     new() = SampleHolder None
-    member val Sample : Sample option = None with get, set
+    member val Sample : Sample option = sample with get, set
+
 
     member this.Apply (event: Event) : SampleHolder =
         match event with
         | NewSample sample ->
-            // log.
             printfn "new sample %O" sample
             this.Sample <- Some sample
             this
