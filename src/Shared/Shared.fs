832b09b61ea16c3930aec760322ef6c1374d5bb1
Giulio Ungaretti <giulioungaretti@me.com>
2019-04-12 21:51:27 +0200

    fix: cleanup unused json dep

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -1,11 +1,4 @@
 namespace Shared
-// By adding this condition, you can share your code between your client and server
-#if FABLE_COMPILER
-open Thoth.Json
-#else
-open Thoth.Json.Net
-#endif
-
 
 [<CLIMutable>]
 type Sample =
